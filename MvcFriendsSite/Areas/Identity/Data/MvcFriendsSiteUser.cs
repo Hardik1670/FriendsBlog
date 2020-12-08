4767f1df94f52c15564fba72f9c370a4fce7e90e
Hardik1670 <66189769+Hardik1670@users.noreply.github.com>
2020-12-07 21:43:35 -0500

    Friends Blog

---
+++ MvcFriendsSite/Areas/Identity/Data/MvcFriendsSiteUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace MvcFriendsSite.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the MvcFriendsSiteUser class
+    public class MvcFriendsSiteUser : IdentityUser
+    {
+    }
+}
---
+++ MvcFriendsSite/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MvcFriendsSite.Areas.Identity.Data;
+using MvcFriendsSite.Data;
+
+[assembly: HostingStartup(typeof(MvcFriendsSite.Areas.Identity.IdentityHostingStartup))]
+namespace MvcFriendsSite.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<MvcFriendsSiteIdentityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("MvcFriendsSiteIdentityContextConnection")));
+
+                services.AddDefaultIdentity<MvcFriendsSiteUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<MvcFriendsSiteIdentityContext>();
+            });
+        }
+    }
+}
